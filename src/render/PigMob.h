bfc0b5c2ffabd1fc1254af295dc41192244a9a30
tohanchik <41820579+tohanchik@users.noreply.github.com>
2026-03-28 17:48:35 +0100

    Fix pig invisibility by forcing opaque texture and disabling cull

---
+++ src/render/PigMob.h
@@ -0,0 +1,27 @@
+#pragma once
+
+class Level;
+class TextureAtlas;
+
+struct PigMob {
+  float x, y, z;
+  float vx, vy, vz;
+
+  float bodyYawDeg;
+  float headYawDeg;
+  float headPitchDeg;
+  float walkAnim;
+
+  float aiTimer;
+  float lookTimer;
+  float targetYawDeg;
+  float moveSpeed;
+
+  bool onGround;
+};
+
+void PigMob_Init(PigMob *pig, Level *level, int seed, float spawnX, float spawnZ);
+void PigMob_Update(PigMob *pig, Level *level, float dt, float playerX, float playerY,
+                   float playerZ, float playerYawDeg);
+void PigMob_Render(const PigMob *pig, TextureAtlas *terrainAtlas);
+void PigMob_Shutdown();
