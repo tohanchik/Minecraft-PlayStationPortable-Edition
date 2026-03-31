5ed47233a9d536961658cce563935c374de15e05
tohanchik <41820579+tohanchik@users.noreply.github.com>
2026-03-31 22:44:17 +0200

    Force single best-direction water routing when pit path exists

--- src/world/chunk_defs.h
+++ src/world/chunk_defs.h
@@ -4,8 +4,10 @@
 
 #define CHUNK_SIZE_X 16
 #define CHUNK_SIZE_Y \
-  64 // 64 blocks height - enough for normal hills
+  128 // MCPE-like world height
 #define CHUNK_SIZE_Z 16
+
+#define SUBCHUNK_COUNT (CHUNK_SIZE_Y / 16)
 
 #define WORLD_CHUNKS_X 8 // 8x8=64 chunks, vertex bufs ~14MB total
 #define WORLD_CHUNKS_Z 8
