38ec5e82511e8894e822e490a76af6add67bc8ac
tohanchik <41820579+tohanchik@users.noreply.github.com>
2026-04-01 08:55:41 +0200

    Defer global block-light rebuilds during bulk updates

--- src/world/Level.h
+++ src/world/Level.h
@@ -96,6 +96,8 @@ private:
   void scheduleLavaTick(int wx, int wy, int wz, int delayTicks);
   void wakeLavaNeighborhood(int wx, int wy, int wz, int delayTicks);
   void processLavaCell(int wx, int wy, int wz);
+  void recomputeBlockLightingFromSources();
+  int getLightAttenuation(uint8_t blockId) const;
 
   Chunk *m_chunks[WORLD_CHUNKS_X][WORLD_CHUNKS_Z];
   std::vector<uint8_t> m_waterDepth;
@@ -124,4 +126,6 @@ private:
   int m_lavaWakeRadius = 8;
   int m_lavaWakeTicks = 0;
   bool m_inWaterSimUpdate = false;
+  bool m_suspendLightingUpdates = false;
+  bool m_pendingBlockLightRebuild = false;
 };
