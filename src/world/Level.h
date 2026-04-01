444ea9507996cf3d7722ae7b9609dd7a01466901
tohanchik <41820579+tohanchik@users.noreply.github.com>
2026-04-01 08:17:56 +0200

    Render lava fluid faces through emissive pass consistently

--- src/world/Level.h
+++ src/world/Level.h
@@ -29,6 +29,7 @@ public:
   uint8_t getBlock(int wx, int wy, int wz) const;
   void setBlock(int wx, int wy, int wz, uint8_t id);
   uint8_t getWaterDepth(int wx, int wy, int wz) const;
+  uint8_t getLavaDepth(int wx, int wy, int wz) const;
   void setWaterDepth(int wx, int wy, int wz, uint8_t depth);
 
   uint8_t getSkyLight(int wx, int wy, int wz) const;
@@ -67,6 +68,7 @@ public:
   int getDay() const { return (int)(m_time / TICKS_PER_DAY); }
 
   long long getTime() const { return m_time; }
+  void setTime(long long t) { m_time = t; }
 
   void tick();
   void setSimulationFocus(int wx, int wy, int wz, int radius);
@@ -85,15 +87,23 @@ private:
 
   void tickWater();
   bool isWaterBlock(uint8_t id) const;
+  bool isLavaBlock(uint8_t id) const;
   int waterIndex(int wx, int wy, int wz) const;
   void scheduleWaterTick(int wx, int wy, int wz, int delayTicks);
   void wakeWaterNeighborhood(int wx, int wy, int wz, int delayTicks);
   void processWaterCell(int wx, int wy, int wz);
+  void tickLava();
+  void scheduleLavaTick(int wx, int wy, int wz, int delayTicks);
+  void wakeLavaNeighborhood(int wx, int wy, int wz, int delayTicks);
+  void processLavaCell(int wx, int wy, int wz);
 
   Chunk *m_chunks[WORLD_CHUNKS_X][WORLD_CHUNKS_Z];
   std::vector<uint8_t> m_waterDepth;
   std::priority_queue<WaterTickNode, std::vector<WaterTickNode>, std::greater<WaterTickNode>> m_waterTicks;
   std::vector<int> m_waterDue;
+  std::vector<uint8_t> m_lavaDepth;
+  std::priority_queue<WaterTickNode, std::vector<WaterTickNode>, std::greater<WaterTickNode>> m_lavaTicks;
+  std::vector<int> m_lavaDue;
   long long m_time = 6000LL;
   float m_lastSunBrightness = 1.0f;
   int m_simFocusX = -1;
@@ -107,5 +117,11 @@ private:
   int m_waterWakeZ = -1;
   int m_waterWakeRadius = 12;
   int m_waterWakeTicks = 0;
+  bool m_lavaDirty = true;
+  int m_lavaWakeX = -1;
+  int m_lavaWakeY = -1;
+  int m_lavaWakeZ = -1;
+  int m_lavaWakeRadius = 8;
+  int m_lavaWakeTicks = 0;
   bool m_inWaterSimUpdate = false;
 };
